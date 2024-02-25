c1685c1434952915896e3dd6bec55f6bedf299c7
lichenhui <lichenhui821@qq.com>
2024-02-26 01:31:54 +0800

    feat: cors

--- user/initialize/router.go
+++ user/initialize/router.go
@@ -2,12 +2,14 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-apis/user/middlewares"
 	userRouter "go-apis/user/router"
 )
 
 // Routers 初始化路由
 func Routers() *gin.Engine {
 	Router := gin.Default()
+	Router.Use(middlewares.Cors())
 	ApiGroup := Router.Group("/user/v1")
 	userRouter.InitUserRouter(ApiGroup)
 	return Router
